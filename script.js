c3994adfbdb5864dae9104482800ae39ef0415e4
alexander sviridov <sviridovalexander81@gmail.com>
2021-11-28 23:27:28 +0100

    add lesson02

---
+++ script.js
@@ -0,0 +1,33 @@
+let title;
+title = 'Lesson02';
+console.log(title);
+
+let screens;
+screens = 'Простые, Сложные, Интерактивные';
+console.log(screens.length);
+console.log(screens.toLocaleLowerCase());
+console.log(screens.split(''));
+
+
+let screenPrice;
+screenPrice = 1;
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+
+let rollback;
+rollback = 50;
+
+let fullPrice;
+fullPrice = 10000000;
+console.log(fullPrice);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+
+let adaptive;
+adaptive = 'false';
+console.log(adaptive);
+
+console.log(fullPrice * (rollback / 100));
+
+
+
+
+
