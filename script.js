e90119b4c5124af1825a22c2481757c9bd9fe9d5
alexander sviridov <sviridovalexander81@gmail.com>
2021-11-29 22:07:38 +0100

    Error correction

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-let title;
-title = 'Lesson02';
-console.log(title);
+let title = 'Lesson02';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 1;
+let rollback = 50;
+let fullPrice = 10000000;
+let adaptive = 'false';
 
-let screens;
-screens = 'Простые, Сложные, Интерактивные';
+//Определение типа данных
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+//Длина строки
 console.log(screens.length);
+
+//Вывод стоимости
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+
+//Приведение строки к нижнему регистру и вывод массива в консоль
 console.log(screens.toLocaleLowerCase());
 console.log(screens.split(''));
 
-
-let screenPrice;
-screenPrice = 1;
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-
-let rollback;
-rollback = 50;
-
-let fullPrice;
-fullPrice = 10000000;
-console.log(fullPrice);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
-
-let adaptive;
-adaptive = 'false';
-console.log(adaptive);
-
+//Процент отката 
 console.log(fullPrice * (rollback / 100));
 
 
