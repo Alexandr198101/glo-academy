4b2a4d3440fd60db0fc97738e2e5ab686e4e8a67
alexander sviridov <sviridovalexander81@gmail.com>
2021-12-04 00:04:24 +0100

    add js in  lesson04

--- script.js
+++ script.js
@@ -1,103 +1,71 @@
 'use strict';
-//Задание 3
+ 
 let title = prompt('Как называется ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?',
-  'Простые, Сложные, Интерактивные');
+let screens = prompt('Какие типы экранов нужно разработать?','Простые, Сложные, Интерактивные');
 let screensPrice = +prompt('Сколько будет стоить данная работа?');
-let adaptive = confirm("Нужен ли адаптив на сайте?");
-let answer1 = prompt(
-  'Какой дополнительный тип услуги нужен?',
- "Макет", "Верстка");
-let answer2 = +prompt('Сколько это будет стоить?', 100);
-let answer3 = prompt(
-  'Какой дополнительный тип услуги нужен?', 'Макет', 'Верстка');
-let answer4 = +prompt('Сколько это будет стоить?',120);
-let fullPrice = screensPrice + answer2 + answer4;
-//let servicePercentPrice = fullPrice - Math.ceil(fullPrice * 0.05);
+let adaptive = confirm('Нужен ли адаптив на сайте?');
 
-/////////
+let answer1 = prompt('Какой дополнительный тип услуги нужен?','Макет','Верстка');
+let answer2 = +prompt("Сколько это будет стоить?");
+let answer3 = prompt('Какой дополнительный тип услуги нужен?','Макет','Верстка');
+let answer4 = +prompt("Сколько это будет стоить?");
 
-let allServicePrices = function (a, b) {
-  return a + b;
+let fullPrice;
+let servicePercentPrice;
+let allServicePrices;
+let commission = 5;
+
+//showTypeOf
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
 };
-console.log(allServicePrices(answer2, answer4));
 
+//getAllServicePrices
+let getAllServicePrices = function () {
+  return answer2 + answer4;
+};
 
-function getfullPrice(a, b) {
-  return a + b;
-}
-getfullPrice(screensPrice,allServicePrices);
+//getFullPrice
+let getFullPrice = function() {
+  return screensPrice + allServicePrices;
+};
 
-
+//getTitle
 function getTitle() {
   console.log(title[0].toUpperCase() + title.substring(1));
-} 
-getTitle();
+}
 
-let servicePercentPrice = function (a, b) {
-  return a - (b * 0.05);
+//getServicePercentPrice
+let getServicePercentPrice = function () {
+  return fullPrice - (fullPrice * (commission / 100));
 };
-console.log(servicePercentPrice(fullPrice, fullPrice * 0.05));
 
-
-
-if (fullPrice >= 30000){
-  alert('Даем скидку в 10%'); 
-} else if (fullPrice >= 15000 && fullPrice < 30000){
-  alert('Даем скидку в 5%');
-} 
-  else if (fullPrice < 15000 && fullPrice >= 0) {
-    alert('Скидка не предусмотрена');
+//getRollbackMessage;
+const getRollbackMessage = function (price) {
+if (price >= 30000) {
+  return'Даем скидку в 10%';
+} else if (price >= 15000 && price < 30000) {
+  return 'Даем скидку в 5%';
+} else if (price < 15000 && price >= 0) {
+  return 'Скидка не предусмотрена';
 } else {
-    alert('Что то пошло не так');
-} 
+  return 'Что то пошло не так';
+}
+};
 
-console.log(title);
-console.log(screens[0]);
-console.log(screensPrice);
-console.log(adaptive);
-console.log(answer1);
-console.log(answer2);
-console.log(answer3);
-console.log(answer4);
+showTypeOf(title);
+showTypeOf(screensPrice);
+
+title = getTitle();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrice();
+allServicePrices = getAllServicePrices();
+
+console.log(getRollbackMessage(fullPrice));
+console.log(servicePercentPrice);
+
+console.log(allServicePrices);
+
 console.log(fullPrice);
-//console.log(servicePercentPrice);
-
-
-//Переменные 
-
-//let title = 'Lesson02';
-//let screens = 'Простые, Сложные, Интерактивные';
-//let screenPrice = 1;
-//let rollback = 50;
-//let fullPrice = 10000000;
-//let adaptive = 'false';
-
-//Определение типа данных
-
-//console.log(typeof title);
-//console.log(typeof fullPrice);
-//console.log(typeof adaptive);
-
-//Длина строки
-//console.log(screens.length);
-
-//Вывод стоимости
-//console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-//console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
-
-//Приведение строки к нижнему регистру и вывод массива в консоль
-//console.log(screens.toLocaleLowerCase().split(", "));
-
-//Процент отката 
-//console.log(fullPrice * (rollback / 100));
-
-
-
-
-
-
-
-
 
 
