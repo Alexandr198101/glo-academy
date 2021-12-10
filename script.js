1a059f085503581968048b71149c133bc049f856
alexander sviridov <sviridovalexander81@gmail.com>
2021-12-10 23:27:27 +0100

    add lesson05

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 'use strict';
  
-let title = prompt('Как называется ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?','Простые, Сложные, Интерактивные');
-let screensPrice = +prompt('Сколько будет стоить данная работа?');
-let adaptive = confirm('Нужен ли адаптив на сайте?');
-
-let answer1 = prompt('Какой дополнительный тип услуги нужен?','Макет','Верстка');
-let answer2 = +prompt("Сколько это будет стоить?");
-let answer3 = prompt('Какой дополнительный тип услуги нужен?','Макет','Верстка');
-let answer4 = +prompt("Сколько это будет стоить?");
-
+let title;
+let screens;
+let screensPrice;
+let adaptive;
 let fullPrice;
 let servicePercentPrice;
 let allServicePrices;
 let commission = 5;
+let answer1;
+let answer3;
+
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+//asking 
+const asking = function () {
+  title = prompt('Как называется ваш проект?');
+  screens = prompt('Какие типы экранов нужно разработать?',
+    'Простые, Сложные, Интерактивные');
+  screensPrice = prompt('Сколько будет стоить данная работа?');
+
+  do {
+    screensPrice = prompt('Сколько будет стоить данная работа?');
+  }
+  while (!isNumber(screensPrice));
+
+  adaptive = confirm('Нужен ли адаптив на сайте?');
+};
 
 //getAllServicePrices
 let getAllServicePrices = function () {
-  return answer2 + answer4;
+
+  let sum = 0;
+
+  for (let i = 0; i < 2; i++) {
+    if (i === 0) {
+      answer1 = prompt(
+        'Какой дополнительный тип услуги нужен?');
+    } else if (i === 1) {
+      answer3 = prompt(
+        'Какой дополнительный тип услуги нужен?');
+    }
+    sum += +prompt('Сколько это будет стоить?');
+    while(!isNumber(sum)){
+      sum = prompt('Сколько это будет стоить?');
+    }
+  }
+  return sum;
 };
 
 //showTypeOf
@@ -54,6 +84,7 @@ if (price >= 30000) {
 }
 };
 
+asking();
 showTypeOf(title);
 showTypeOf(screensPrice);
 showTypeOf(adaptive);
@@ -63,6 +94,7 @@ fullPrice = getFullPrice();
 servicePercentPrice = getServicePercentPrice();
 title = getTitle();
 
+console.log("allServicePrices", allServicePrices);
 console.log(getRollbackMessage(fullPrice));
 console.log(typeof 'title');
 console.log(typeof screensPrice);
@@ -70,7 +102,7 @@ console.log(typeof adaptive);
 
 console.log(screens.length);
 console.log(servicePercentPrice);
-//console.log(title[0].toUpperCase() + title.substring(1).toLowerCase());
+
 
 
 
